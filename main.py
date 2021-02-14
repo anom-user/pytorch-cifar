661de1074a25cb2f60191d85cb461628e7047d61
anom-user <78714524+anom-user@users.noreply.github.com>
2021-02-14 15:50:10 +1100

    Update main.py

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ def test(epoch):
         best_acc = acc
 
 
-for epoch in range(start_epoch, start_epoch+100): #start_epoch+200):
+for epoch in range(start_epoch, start_epoch+200): #start_epoch+200):
     train(epoch)
     test(epoch)
     scheduler.step()
